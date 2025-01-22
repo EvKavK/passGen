0061d3bbbc423ea14ac6553a8b82000e7f82fb32
EvKavK <evan.kavanagh@keyin.com>
2025-01-22 19:25:50 -0330

    Fixed a bug involving --length. Also added more comments for clarification's sake.

--- index.js
+++ index.js
@@ -3,22 +3,25 @@
 // Purpose: A simple, random lower-case password generator with error handling.
 
 const process = require("process");
-const args = process.argv.slice(2);
+const args = process.argv.slice(2); // ignores 'node' and 'index.js'
 
-const passLowAlph = "abcdefghijklmnopqrstuvwxyz";
+const passLowAlph = "abcdefghijklmnopqrstuvwxyz"; // lowercase whitelist
 
+// default password length, the minimum, and the maximum.
 let passLength = 8;
 let passMin = 6
 let passMax = 16
+
 let lengthIndex;
 
-let passPool = passLowAlph;
+let passPool = passLowAlph; // the password character pool will always have lowercases in them.
 
 const commands = [
     "--help",
     "--length"
 ]
 
+// calls for help, closes upon being called
 if (args.includes("--help")) {
     help()
     process.exit(1)
@@ -47,8 +50,9 @@ function help() {
     console.log("|---------------------------------------------------------------------------------------|");
 };
 
+// adjusts the length based on what the user inputs in the --length argument.
 function length() {
-    //
+    // checks the argument ahead of --length whenever it is called and turns it into a readable integer.
     lengthIndex = args.indexOf("--length");
     passLength = parseInt(args[lengthIndex + 1], 10);
 
@@ -62,15 +66,17 @@ function length() {
     };
 };
 
+// the main function.
 function passGen() {
     args.forEach(function(arg, index) {
-        // checks if the command provided is part of the command whitelist, but also ignores numbers that are not part of length.
-        if (!commands.includes(arg) && (index === 0 || !args[lengthIndex + 1])) {
+        // checks if the command provided is part of the command whitelist, but also detects numbers that are not part of length.
+        if (!commands.includes(arg) && arg !== args[lengthIndex + 1]) {
             console.log("Error! Invalid command! Please refer to --help in order to get a list of valid commands!")
             process.exit(0)
         };
     });
 
+    // creates a totally random password based on the whitelist provided by passPool
     let password = "";
 
     for (let i = 0; i < passLength; i++) { 
