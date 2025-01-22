d63251de7cb283f6e6f2e8904a4528d14cec6603
EvKavK <evan.kavanagh@keyin.com>
2025-01-22 18:59:18 -0330

    Added the length feature, as well as error checks. Removed mentions of additional arguments.

--- index.js
+++ index.js
@@ -2,49 +2,31 @@
 // Date: January 20th, 2025
 // Purpose: A simple, random lower-case password generator with error handling.
 
-// Things to add: (maybe run command), help command, lower command, upper command, special command, length command (8-16)
-// -lower, upper, and special will enable/disable lowercase, uppercase, and certain special characters in password generation
-// -length will allow the user to create longer passwords, at the lowest 8 characters, at the highest 16 characters
-// Bugs to consider:
-// -if lower, upper, and special are disabled, will the password break or just add nothing? either way, an error message should appear.
-// if everything is working accordingly, congrats! you're done! :D
-
 const process = require("process");
 const args = process.argv.slice(2);
 
+const passLowAlph = "abcdefghijklmnopqrstuvwxyz";
+
+let passLength = 8;
+let passMin = 6
+let passMax = 16
+let lengthIndex;
+
+let passPool = passLowAlph;
+
+const commands = [
+    "--help",
+    "--length"
+]
+
 if (args.includes("--help")) {
     help()
-};
-
-if (args.includes("--lower")) {
-    lower()
-};
-
-if (args.includes("--upper")) {
-    upper()
-};
-
-if (args.includes("--special")) {
-    special()
+    process.exit(1)
 };
 
 if (args.includes("--length")) {
     length()
 };
-
-function passGen() {
-    let passLength = 8;
-    const passLowAlph = "abcdefghijklmnopqrstuvwxyz";
-    let password = "";
-
-    for (let i = 0; i < passLength; i++) { 
-        password += passLowAlph[Math.floor(Math.random() * passLowAlph.length)]
-    }
-    return password;
-};
-
-console.log(passGen());
-
 
 function help() {
     console.log("|---------------------------------------------------------------------------------------|");
@@ -59,12 +41,42 @@ function help() {
     console.log("|                                                                                       |");
     console.log("|---------------------------------------------------------------------------------------|");
     console.log("|                                                                                       |");
-    console.log("| --help    - Displays the help menu (you are here!)                                    |");
-    console.log("| --lower   - Enables and/or disables lowercase characters in password generation.      |");
-    console.log("| --upper   - Enables and/or disables uppercase characters in password generation.      |");
-    console.log("| --special - Enables and/or disables special characters in password generation.        |");
-    console.log("| --length  - Adjusts the length of the password from anywhere between 8-16 characters. |");
+    console.log("| --help       - Displays the help menu (you are here!)                                 |");
+    console.log("| --length (#) - Adjusts the password's length from anywhere between 8-16 characters.   |");
     console.log("|                                                                                       |");
     console.log("|---------------------------------------------------------------------------------------|");
 };
 
+function length() {
+    //
+    lengthIndex = args.indexOf("--length");
+    passLength = parseInt(args[lengthIndex + 1], 10);
+
+    // checks if the argument ahead of --length is a valid number that adheres to the set minimum and maximum length.
+    if (isNaN(passLength)) {
+        console.log("Error! Invalid length! Please provide a valid number for length and try again!")
+        process.exit(0)
+    } else if (passLength < passMin || passLength > passMax) {
+        console.log("Error! Invalid length! Please respect the character limit (6-16) and try again!")
+        process.exit(0)
+    };
+};
+
+function passGen() {
+    args.forEach(function(arg, index) {
+        // checks if the command provided is part of the command whitelist, but also ignores numbers that are not part of length.
+        if (!commands.includes(arg) && (index === 0 || !args[lengthIndex + 1])) {
+            console.log("Error! Invalid command! Please refer to --help in order to get a list of valid commands!")
+            process.exit(0)
+        };
+    });
+
+    let password = "";
+
+    for (let i = 0; i < passLength; i++) { 
+        password += passPool[Math.floor(Math.random() * passPool.length)]
+    }
+    return password;
+};
+
+console.log(passGen());
